4b5867ad7c84a104040a0dd30b812c80f47739b8
algesuar <34496162+algesuar@users.noreply.github.com>
2021-10-03 22:50:57 -0500

    Consula

---
+++ src/components/editComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <form @submit.prevent="methodEnviar">
+        <label>String cinema:</label> 
+        <input type="text" v-model="cinema.strcinema" > <br> 
+
+        <button> Entrar datos </button>
+    </form>
+</template>
+<script>
+   import axios from "axios"; 
+   export default{
+       data(){
+          return{
+              cinema:{
+                  strsillas:"1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+              }
+          }
+       },
+       created() {
+         let apiURL = `http://localhost:8000/actualizaSillas/${this.$route.params.id}`;
+         axios.get(apiURL).then((res) => {
+         this.cinema = res.data;
+ });
+ },
+   }
+</script>   
---
+++ src/components/listComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1> Listar </h1>
+       {{ Cinemas.strcinema }}
+  </div>
+</template>
+
+<script>
+   import axios from "axios";
+   export default{
+        data(){
+           return{
+               strcinema: []
+           }
+        },
+
+        created(){
+            axios.get("http://localhost:8000/consultaSillas").then((res) => this.strcinema=res.data);
+
+       },
+       methods: {
+       }  
+     }
+</script>
