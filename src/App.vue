ddee2ff66446f60efaae1b00c4851e04618b9a0a
algesuar <34496162+algesuar@users.noreply.github.com>
2021-10-06 08:33:51 -0500

    Cambios en el renderizado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,168 @@
 <template>
   <div id="app">
-     <div>
-       <router-view>
-       </router-view>  
-     </div>  
-  </div>
+ <!--//<div>
+     //  <router-view>
+     //  </router-view>  
+     //</div>
+ -->
+
+<h1>CINEMA</h1>
+<hr>
+<pre>
+     A1          A2          A3          A4          A5          A6         A7          A8          A9          A10   
+</pre>
+<IMG src="silla-vip40.png" id="A1" onclick="changeImage2('A1',0)"  />
+<!--  <IMG src="silla1.png">    -->
+<IMG src="silla-vip40.png" id="A2" onclick="changeImage2('A2',1)"  />
+<IMG src="silla-vip40.png" id="A3" onclick="changeImage2('A3',2)"  />
+<IMG src="silla-vip40.png" id="A4" onclick="changeImage2('A4',3)"  />
+<IMG src="silla-vip40.png" id="A5" onclick="changeImage2('A5',4)"  />
+<IMG src="silla-vip40.png" id="A6" onclick="changeImage2('A6',5)"  />
+<IMG src="silla-vip40.png" id="A7" onclick="changeImage2('A7',6)"  />
+<IMG src="silla-vip40.png" id="A8" onclick="changeImage2('A8',7)"  />
+<IMG src="silla-vip40.png" id="A9" onclick="changeImage2('A9',8)"  />
+<IMG src="silla-vip40.png" id="A10" onclick="changeImage2('A10',9)"  />
+<br>
+<pre>
+     B1          B2          B3          B4          B5          B6         B7          B8          B9          B10   
+
+</pre> 
+<IMG src="silla-vip40.png" id="B1" onclick="changeImage2('B1',10)"  />
+<IMG src="silla-vip40.png" id="B2" onclick="changeImage2('B2',11)"  />
+<IMG src="silla-vip40.png" id="B3" onclick="changeImage2('B3',12)"  />
+<IMG src="silla-vip40.png" id="B4" onclick="changeImage2('B4',13)"  />
+<IMG src="silla-vip40.png" id="B5" onclick="changeImage2('B5',14)"  />
+<IMG src="silla-vip40.png" id="B6" onclick="changeImage2('B6',15)"  />
+<IMG src="silla-vip40.png" id="B7" onclick="changeImage2('B7',16)"  />
+<IMG src="silla-vip40.png" id="B8" onclick="changeImage2('B8',17)"  />
+<IMG src="silla-vip40.png" id="B9" onclick="changeImage2('B9',18)"  />
+<IMG src="silla-vip40.png" id="B10" onclick="changeImage2('B10',19)"  />
+<br>
+<pre>
+     C1          C2          C3          C4          C5          C6         C7          C8          C9          C10   
+</pre>
+<IMG src="silla-vip40.png" id="C1" onclick="changeImage2('C1',20)"  />
+<IMG src="silla-vip40.png" id="C2" onclick="changeImage2('C2',21)"  />
+<IMG src="silla-vip40.png" id="C3" onclick="changeImage2('C3',22)"  />
+<IMG src="silla-vip40.png" id="C4" onclick="changeImage2('C4',23)"  />
+<IMG src="silla-vip40.png" id="C5" onclick="changeImage2('C5',24)"  />
+<IMG src="silla-vip40.png" id="C6" onclick="changeImage2('C6',25)"  />
+<IMG src="silla-vip40.png" id="C7" onclick="changeImage2('C7',26)"  />
+<IMG src="silla-vip40.png" id="C8" onclick="changeImage2('C8',27)"  />
+<IMG src="silla-vip40.png" id="C9" onclick="changeImage2('C9',28)"  />
+<IMG src="silla-vip40.png" id="C10" onclick="changeImage2('C10',29)"  />
+<br>
+<pre>
+     D1          D2          D3          D4          D5          D6         D7          D8          D9          D10   
+ </pre>
+<IMG src="silla11.png" id="D1" onclick="changeImage('D1',30)"  />
+<IMG src="silla11.png" id="D2" onclick="changeImage('D2',31)"  />
+<IMG src="silla11.png" id="D3" onclick="changeImage('D3',32)"  />
+<IMG src="silla11.png" id="D4" onclick="changeImage('D4',33)"  />
+<IMG src="silla11.png" id="D5" onclick="changeImage('D5',34)"  />
+<IMG src="silla11.png" id="D6" onclick="changeImage('D6',35)"  />
+<IMG src="silla11.png" id="D7" onclick="changeImage('D7',36)"  />
+<IMG src="silla11.png" id="D8" onclick="changeImage('D8',37)"  />
+<IMG src="silla11.png" id="D9" onclick="changeImage('D9',38)"  />
+<IMG src="silla11.png" id="D10" onclick="changeImage('D10',39)"  />
+<br>
+<pre>
+     E1          E2          E3          E4          E5          E6         E7          E8          E9          E10   
+</pre>
+<IMG src="silla11.png" id="E1" onclick="changeImage('E1',40)"  />
+<IMG src="silla11.png" id="E2" onclick="changeImage('E2',41)"  />
+<IMG src="silla11.png" id="E3" onclick="changeImage('E3',42)"  />
+<IMG src="silla11.png" id="E4" onclick="changeImage('E4',43)"  />
+<IMG src="silla11.png" id="E5" onclick="changeImage('E5',44)"  />
+<IMG src="silla11.png" id="E6" onclick="changeImage('E6',45)"  />
+<IMG src="silla11.png" id="E7" onclick="changeImage('E7',46)"  />
+<IMG src="silla11.png" id="E8" onclick="changeImage('E8',47)"  />
+<IMG src="silla11.png" id="E9" onclick="changeImage('E9',48)"  />
+<IMG src="silla11.png" id="E10" onclick="changeImage('E10',49)"  />
+<br>
+<pre>
+     F1          F2          F3          F4          F5          F6         F7          F8          F9          F10   
+</pre>
+<IMG src="silla11.png" id="F1" onclick="changeImage('F1',50)"  />
+<IMG src="silla11.png" id="F2" onclick="changeImage('F2',51)"  />
+<IMG src="silla11.png" id="F3" onclick="changeImage('F3',52)"  />
+<IMG src="silla11.png" id="F4" onclick="changeImage('F4',53)"  />
+<IMG src="silla11.png" id="F5" onclick="changeImage('F5',54)"  />
+<IMG src="silla11.png" id="F6" onclick="changeImage('F6',55)"  />
+<IMG src="silla11.png" id="F7" onclick="changeImage('F7',56)"  />
+<IMG src="silla11.png" id="F8" onclick="changeImage('F8',57)"  />
+<IMG src="silla11.png" id="F9" onclick="changeImage('F9',58)"  />
+<IMG src="silla11.png" id="F10" onclick="changeImage('F10',59)"  />
+<br>
+<pre>
+     G1          G2          G3          G4          G5          G6         G7          G8          G9          G10   
+</pre>
+<IMG src="silla11.png" id="G1" onclick="changeImage('G1',60)"  />
+<IMG src="silla11.png" id="G2" onclick="changeImage('G2',61)"  />
+<IMG src="silla11.png" id="G3" onclick="changeImage('G3',62)"  />
+<IMG src="silla11.png" id="G4" onclick="changeImage('G4',63)"  />
+<IMG src="silla11.png" id="G5" onclick="changeImage('G5',64)"  />
+<IMG src="silla11.png" id="G6" onclick="changeImage('G6',65)"  />
+<IMG src="silla11.png" id="G7" onclick="changeImage('G7',66)"  />
+<IMG src="silla11.png" id="G8" onclick="changeImage('G8',67)"  />
+<IMG src="silla11.png" id="G9" onclick="changeImage('G9',68)"  />
+<IMG src="silla11.png" id="G10" onclick="changeImage('G10',69)"  />
+<br>
+<pre>
+     H1          H2          H3          H4          H5          H6         H7          H8          H9          H10   
+</pre>
+<IMG src="silla11.png" id="H1" onclick="changeImage('H1',70)"  />
+<IMG src="silla11.png" id="H2" onclick="changeImage('H2',71)"  />
+<IMG src="silla11.png" id="H3" onclick="changeImage('H3',72)"  />
+<IMG src="silla11.png" id="H4" onclick="changeImage('H4',73)"  />
+<IMG src="silla11.png" id="H5" onclick="changeImage('H5',74)"  />
+<IMG src="silla11.png" id="H6" onclick="changeImage('H6',75)"  />
+<IMG src="silla11.png" id="H7" onclick="changeImage('H7',76)"  />
+<IMG src="silla11.png" id="H8" onclick="changeImage('H8',77)"  />
+<IMG src="silla11.png" id="H9" onclick="changeImage('H9',78)"  />
+<IMG src="silla11.png" id="H10" onclick="changeImage('H10',79)"  />
+<br>
+<pre>
+     I1          I2          I3          I4          I5          I6         I7          I8          I9          I10   
+</pre>
+<IMG src="silla11.png" id="I1" onclick="changeImage('I1',80)"  />
+<IMG src="silla11.png" id="I2" onclick="changeImage('I2',81)"  />
+<IMG src="silla11.png" id="I3" onclick="changeImage('I3',82)"  />
+<IMG src="silla11.png" id="I4" onclick="changeImage('I4',83)"  />
+<IMG src="silla11.png" id="I5" onclick="changeImage('I5',84)"  />
+<IMG src="silla11.png" id="I6" onclick="changeImage('I6',85)"  />
+<IMG src="silla11.png" id="I7" onclick="changeImage('I7',86)"  />
+<IMG src="silla11.png" id="I8" onclick="changeImage('I8',87)"  />
+<IMG src="silla11.png" id="I9" onclick="changeImage('I9',88)"  />
+<IMG src="silla11.png" id="I10" onclick="changeImage('I10',89)"  />
+<br>
+<pre>
+     J1          J2          J3          J4          J5          J6         J7          J8          J9          J10   
+ </pre>
+<IMG src="silla11.png" id="J1" onclick="changeImage('J1',90)"  />
+<IMG src="silla11.png" id="J2" onclick="changeImage('J2',91)"  />
+<IMG src="silla11.png" id="J3" onclick="changeImage('J3',92)"  />
+<IMG src="silla11.png" id="J4" onclick="changeImage('J4',93)"  />
+<IMG src="silla11.png" id="J5" onclick="changeImage('J5',94)"  />
+<IMG src="silla11.png" id="J6" onclick="changeImage('J6',95)"  />
+<IMG src="silla11.png" id="J7" onclick="changeImage('J7',96)"  />
+<IMG src="silla11.png" id="J8" onclick="changeImage('J8',97)"  />
+<IMG src="silla11.png" id="J9" onclick="changeImage('J9',98)"  />
+<IMG src="silla11.png" id="J10" onclick="changeImage('J10',99)"  />
+<br>
+<hr>
+<IMG src="pantalla11.png"/>  
+</div>
+ 
 </template>
 
 <script>
 
-
+/*import changeImage from 'cinemaUtils';*/
+/*import changeImage from './router/cinemaUtils.js';*/
 export default {
   name: 'App',
+  
 }
 </script>
 
@@ -20,7 +171,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/router/cinemaUtils.js
@@ -0,0 +1,23 @@
+function changeImage(nsilla,i) {
+
+            estaSilla = document.getElementById(nsilla).src ;
+    
+            /*if (document.getElementById(nsilla).src == "file:///C:/datos/imagen-mintic/silla2.png") {
+               return;
+            endif
+            */
+            /*if ( estaSilla == "file:///C:/datos/imagen-mintic/silla21.png" ) {
+               return ;
+            endif   
+            }
+            
+            document.getElementById(nsilla).src = "C:/datos/imagen-mintic/silla21.png"
+            */
+            document.getElementById(nsilla).src = "silla21.png"
+            
+            /*sillas.push( nsilla );
+            text += "Reservar: " +nsilla + "<br>";             
+            teatro[i] = 1 ; 
+            document.getElementById("demo").innerHTML = text;              
+            */    
+}
