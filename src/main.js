9efe1c022b9682b45704739520704ff6bf27f1c3
David Felipe Moncada Ramirez <86807540+Dfmrd@users.noreply.github.com>
2021-10-11 19:39:08 -0500

    .

--- src/main.js
+++ src/main.js
@@ -9,5 +9,3 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
-
-//const uri = "mongodb+srv://dfmrd:NJQX7dadAq9d2Q57@cluster0.x7sik.mongodb.net/Carte1?retryWrites=true&w=majority"
